30c05578dd069c2e20e040de8a9e46b4fac0e5af
Kassem DAOUSSI <tacoskebab@hotmail.fr>
2014-03-04 12:19:07 +0100

    missed line (escaping string)

--- static/js/functions.js
+++ static/js/functions.js
@@ -47,6 +47,7 @@ function dismissLogin(){
  */
 function sendMessage(mess) {
           var message = document.getElementById('message').value;
+          var message = htmlEscape(message);
           socket.emit('newPost', { 'hour' : '', 'pseudo' : pseudo, 'message' : message, rank: 4});
           scrollBottom();
           $('#message').val('');
