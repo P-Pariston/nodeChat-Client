1e3f8f3946f995f8d667429364e459187416ff24
Kassem DAOUSSI <tacoskebab@hotmail.fr>
2014-03-22 20:16:32 +0100

    Add login stuffs

--- static/js/checkConnection.js
+++ static/js/checkConnection.js
@@ -1,3 +1,10 @@
+function clearForm(){
+   $('#login').html("<p style='height:50%; margin-top:7px;''><button id='Loginbtn' class='small button' onclick='return false;''>Login</button>  &nbsp;<button id='Registerbtn' class='small button' onclick='return false;''>Register</button>  &nbsp;&nbsp;&nbsp; Hi ! Welcome to nodeChat, please login to post a message.</p>");
+   //Clearing user form
+   $('#user-name').html('');
+   $('#user-form').html('');
+   $('#user').html('Guest');
+}
 socket.on('connected', function(connected) {
       /*
        * NULL: not connected to the node server
@@ -6,7 +13,7 @@ socket.on('connected', function(connected) {
       console.log(connected);
       var connected = connected;
       if(connected == 1){
-       $('#login').html("<p style='height:50%; margin-top:7px;''><button class='small button' onclick='return false;''>Login</button> <!-- &nbsp;<button class='small button' onclick='return false;''>Register</button> --> &nbsp;&nbsp;&nbsp; Hi ! Welcome to nodeChat, please login to post a message.</p>");      
+       $('#login').html("<p style='height:50%; margin-top:7px;''><button id='Loginbtn' class='small button' onclick='return false;''>Login</button>  &nbsp;<button id='Registerbtn' class='small button' onclick='return false;''>Register</button>  &nbsp;&nbsp;&nbsp; Hi ! Welcome to nodeChat, please login to post a message.</p>");      
       }else{}
   });
 
@@ -16,13 +23,31 @@ socket.on('BadName', function(c){
    */
    if(c == 1){
    console.log('The value of c is 1, the name is too long.')
-   $('#login').html("<p style='height:50%; margin-top:7px;''><button class='small button' onclick='return false;''>Login</button> <!-- &nbsp;<button class='small button' onclick='return false;''>Register</button> --> &nbsp;&nbsp;&nbsp; Hi ! Welcome to nodeChat, please login to post a message.</p>");
-   //Clearing user form
-   $('#user-name').html('');
-   $('#user-form').html('');
-   $('#user').html('Guest')
+   clearForm();
    }
-})
+});
+
+socket.on('isLogged', function(c){
+    /*
+     * -1: User doesn't exist
+     *  0: Wrong id/password combinaison
+     *  1: Connected
+     */
+     switch(c){
+      case '-1':
+      clearForm();
+      break;
+      case '0':
+      clearForm();
+      break;
+      case '1':
+      //OK
+      break;
+      default:
+      alert('woops, there is a problem... :/');
+     }
+});
+
   socket.on('disconnect', function () {
     document.getElementById('tchat').innerHTML += '<div id="line"><b>bot</b> : You have been disconnected, possibly because this server was restarted.</div>';
     $('#user-form').remove();
